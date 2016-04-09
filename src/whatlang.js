7acfe2aa57e424b9a0107b6c744969ab4751480e
Elijah El-Haddad <elijahel@gmail.com>
2016-04-09 16:58:40 -0400

    whatlang - error detection algorithm WIP - 4:35PM to 4:59PM [April 9, 2016]

--- src/whatlang.js
+++ src/whatlang.js
@@ -1,14 +1,12 @@
 require("codemirror/lib/codemirror.css");
 var CodeMirror = require("codemirror");
 require("codemirror/addon/runmode/runmode");
+
+// TODO: generate languages array by reading CodeMirror directory
 var languages = ["css", "htmlmixed", "javascript"];
 var modes = languages.map(function(language) {
   return require("codemirror/mode/" + language + "/" + language);
 });
-
-// var modes = modeDirectories.forEach(function(modeDir) {
-//   console.log("modeDir=", modeDir);
-// });
 
 var objectAssign = require('object-assign');
 
@@ -24,7 +22,7 @@ objectAssign(WhatLang.prototype, {
       mode: "javascript"
     });
 
-    this.editor.on("changes", this.onChange.bind(this));
+    this._editor.on("change", this.onChange.bind(this));
   },
   onChange: function(editor, changeObj){
     if (this._detectModeTimeout) {
@@ -33,6 +31,25 @@ objectAssign(WhatLang.prototype, {
     this._detectModeTimeout = window.setTimeout(this.detectMode.bind(this), 1000);
   },
   detectMode: function() {
+    var valueString = this._editor.getValue();
+    var numberOfErrors = [];
+
+    languages.forEach(function(language, languageIndex) {
+      var errorCounter = {
+        language: language,
+        numberOfErrors: 0
+      };
+
+      CodeMirror.runMode(valueString, language, function(currentString, style){
+        if (style && -1 !== style.indexOf("error")) {
+          errorCounter.numberOfErrors++;
+        }
+        console.log("language=", language, "currentString", currentString, " style=", style);
+      }.bind(this));
+
+      numberOfErrors[languageIndex] = errorCounter;
+      console.log("language", errorCounter);
+    }, this)
 
     this._detectModeTimeout = null;
   }
